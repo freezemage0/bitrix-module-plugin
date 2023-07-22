6eb509119e4d651ef7fe794231682c2eb1295f9e
Freezemage <freezemage0@gmail.com>
2023-07-22 03:47:33 +0300

    Fix CommandProvider.

--- src/CommandProvider.php
+++ src/CommandProvider.php
@@ -6,20 +6,31 @@ namespace Freezemage\BitrixPlugin;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\Capability\CommandProvider as CommandProviderCapability;
+use InvalidArgumentException;
 
 
 final class CommandProvider implements CommandProviderCapability
 {
-    public function __construct(
-            private readonly Composer $composer,
-            private readonly IOInterface $io
-    ) {
+    private readonly Composer $composer;
+    private readonly IOInterface $io;
+
+    public function __construct(array $parameters) {
+        if (!isset($parameters['composer']) || !($parameters['composer'] instanceof Composer)) {
+            throw new InvalidArgumentException('Composer not found.');
+        }
+
+        if (!isset($parameters['io']) || !($parameters['io'] instanceof IOInterface)) {
+            throw new InvalidArgumentException('IO not found.');
+        }
+
+        $this->composer = $parameters['composer'];
+        $this->io = $parameters['io'];
     }
 
     public function getCommands(): array
     {
         return [
-                new BootstrapModuleCommand()
+                BootstrapModuleCommand::create($this->composer, $this->io)
         ];
     }
 }
